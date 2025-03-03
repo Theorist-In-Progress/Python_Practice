3ec7360de85d07fe2aa31321508f1634ec25d004
Lalit <sonilalit717@gmail.com>
2025-03-03 19:50:25 +0530

    modified the log files

--- Logging/LOGS/tesst.py
+++ Logging/LOGS/tesst.py
@@ -1,6 +1,6 @@
 
 ### with this file we are checking and learning how it will create logs inside the app.log file which is given as a config in logger.py file from logger import logging 
-
+from logger import logging
 def addition(a,b):
     logging.debug("the addition operation taking place ")
     return a+b
